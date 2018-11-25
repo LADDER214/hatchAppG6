35e707d9bec8141c1a50b1163fa445a260fe0237
Abby Mitchell <abby.mitchell.15@ucl.ac.uk>
2018-11-25 10:31:32 +0000

    re-organised files

--- AppNavigator.js
+++ AppNavigator.js
@@ -1,23 +1,12 @@
 import { createStackNavigator, createAppContainer } from 'react-navigation';
-import HomeScreen from './HomeScreen';
-import EmployerHome from './EmployerHome';
-import EmployeeHome from './EmployeeHome';
-import FindEmployees from './FindEmployees';
-import EmployeeProfile from './EmployeeProfile';
-import Chat from './Chat';
+import HomeScreen from './pages/HomeScreen';
+import EmployerHome from './pages/employer/EmployerHome';
+import EmployeeHome from './pages/employee/EmployeeHome';
+import FindEmployees from './pages/employer/FindEmployees';
+import EmployeeProfile from './pages/employee/EmployeeProfile';
+import Chat from './pages/Chat';
 
-// const AppNavigator = createStackNavigator({
-//   HomeScreen: { screen: HomeScreen },
-//   EmployerHome: {screen: EmployerHome},
-//   EmployeeHome: {screen: EmployeeHome},
-//   FindEmployees: {screen: FindEmployees},
-//   EmployeeProfile: {screen: EmployeeProfile},
-//   Chat: {screen: Chat},
-// });
-
-// export default createAppContainer(AppNavigator);
-
-const  = createStackNavigator({
+const  AppNavigator = createStackNavigator({
   HomeScreen: { screen: HomeScreen },
   EmployerHome: {screen: EmployerHome},
   EmployeeHome: {screen: EmployeeHome},
